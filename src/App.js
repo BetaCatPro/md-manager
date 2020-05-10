d3b228603134eeeeca6a15820f222195efe02f63
BetaCatPro <zj20162325@163.com>
2020-05-10 10:49:04 +0800

    实现上下文菜单及原生菜单

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { faPlus, faFileImport } from '@fortawesome/free-solid-svg-icons'
@@ -11,95 +11,255 @@ import FileList from './components/FileList'
 import BottomBtn from './components/BottomBtn'
 import TabList from './components/TabList'
 
-import defaultFiles from './utils/defaultFiles'
+import { flattenArr, objToArr, timestampToString } from './utils/helper'
+import fileHelper from './utils/fileHelper'
+
+import useIpcRenderer from './hooks/useIpcRenderer'
+// require node.js modules
+const { join, basename, extname, dirname } = window.require('path')
+const { remote, ipcRenderer } = window.require('electron')
+const Store = window.require('electron-store')
+const fileStore = new Store({'name': 'Files Data'})
+const settingsStore = new Store({name: 'Settings'})
+const getAutoSync = () => ['accessKey', 'secretKey', 'bucketName', 'enableAutoSync'].every(key => !!settingsStore.get(key))
+const saveFilesToStore = (files) => {
+    // we don't have to store any info in file system, eg: isNew, body ,etc
+    const filesStoreObj = objToArr(files).reduce((result, file) => {
+        const { id, path, title, createdAt, isSynced, updatedAt } = file
+        result[id] = {
+            id,
+            path,
+            title,
+            createdAt,
+            isSynced,
+            updatedAt
+        }
+        return result
+    }, {})
+    fileStore.set('files', filesStoreObj)
+}
 
 function App() {
-    const [ files, setFiels ] = useState(defaultFiles)
-    const [ searchFiles, setSearchFiels ] = useState([])
-    const [ activeFileId, setActiveFileId ] = useState('')
-    const [ openedFilesId, setOpenedFilesId ] = useState([])
-    const [ unsavedFilesId, setUnsavedFilesId ] = useState([])
-    const openedFiles = openedFilesId.map((openedId) => {
-        return files.find(file => file.id === openedId)
+    const [ files, setFiles ] = useState(fileStore.get('files') || {})
+    const [ activeFileID, setActiveFileID ] =useState('')
+    const [ openedFileIDs, setOpenedFileIDs ] = useState([])
+    const [ unsavedFileIDs, setUnsavedFileIDs ] = useState([])
+    const [ searchedFiles, setSearchedFiles ] = useState([])
+    const [ isLoading, setLoading ] = useState(false)
+    const filesArr = objToArr(files)
+    const savedLocation = settingsStore.get('savedFileLocation') || remote.app.getPath('documents')
+    const activeFile = files[activeFileID]
+
+    const openedFiles = openedFileIDs.map(openID => {
+        return files[openID]
     })
-    const activeFile = files.find(file => file.id === activeFileId)
-    const fileListArr = (searchFiles.length > 0) ? searchFiles : files
 
-    // 搜索文件
-    const fileSearch = (keyword) => {
-        const newFiles = files.filter(file => file.title.includes(keyword))
-        setSearchFiels(newFiles)
-    }
+    const fileListArr = (searchedFiles.length > 0) ? searchedFiles : filesArr
 
-    // 文件列表点击
-    const fileClick = (fileId) => {
-        setActiveFileId(fileId)
-        if(!openedFilesId.includes(fileId)) {
-            setOpenedFilesId([...openedFilesId,fileId])
+    const fileClick = (fileID) => {
+        // set current active file
+        setActiveFileID(fileID)
+        const currentFile = files[fileID]
+        const { id, title, path, isLoaded } = currentFile
+        if (!isLoaded) {
+            if (getAutoSync()) {
+                ipcRenderer.send('download-file', { key: `${title}.md`, path, id })
+            } else {
+                fileHelper.readFile(currentFile.path).then(value => {
+                    const newFile = { ...files[fileID], body: value, isLoaded: true }
+                    setFiles({ ...files, [fileID]: newFile })
+                })
+            }
+        }
+        // if openedFiles don't have the current ID
+        // then add new fileID to openedFiles
+        if (!openedFileIDs.includes(fileID)) {
+            setOpenedFileIDs([ ...openedFileIDs, fileID ])
         }
     }
 
-    // 编辑文件名
-    const saveEdit = (fileId, title) => {
-        const newFiles = files.map(file => {
-            if(file.id === fileId) {
-                file.title = title
-                file.isNew = false
-            }
-            return file
-        })
-        setFiels(newFiles)
+    const tabClick = (fileID) => {
+        // set current active file
+        setActiveFileID(fileID)
     }
 
-    // 删除文件
-    const fileDelete = (fileId) => {
-        const newFiles = files.filter(file => file.id != fileId)
-        setFiels(newFiles)
-        tabClose(fileId)
-    }
-
-    // 点击导航标签
-    const tabClick = (fileId) => {
-        setActiveFileId(fileId)
-    }
-
-    // 关闭导航标签
     const tabClose = (id) => {
-        const tabWithout = openedFilesId.filter(fileID => fileID != id)
-        setOpenedFilesId(tabWithout)
-        if(tabWithout.length > 0) {
-            setActiveFileId(tabWithout[0])
+        //remove current id from openedFileIDs
+        const tabsWithout = openedFileIDs.filter(fileID => fileID !== id)
+        setOpenedFileIDs(tabsWithout)
+        // set the active to the first opened tab if still tabs left
+        if (tabsWithout.length > 0) {
+            setActiveFileID(tabsWithout[0])
         } else {
-            setActiveFileId('')
+            setActiveFileID('')
         }
     }
 
-    // 文档内容改变回调
     const fileChange = (id, value) => {
-        const newFiles = files.map(file => {
-            if(file.id === id) {
-                file.body = value
+        if (value !== files[id].body) {
+            const newFile = { ...files[id], body: value }
+            setFiles({ ...files, [id]: newFile })
+            // update unsavedIDs
+            if (!unsavedFileIDs.includes(id)) {
+                setUnsavedFileIDs([ ...unsavedFileIDs, id])
             }
-            return file
-        })
-        setFiels(newFiles)
-        if(!unsavedFilesId.includes(id)) {
-            setUnsavedFilesId([...unsavedFilesId, id])
         }
     }
 
-    // 新建文件
-    const createFile = () => {
-        const newId = uuidv4()
-        const newFiles = [...files,{
-            id: newId,
-            title: '',
-            body: '## title',
-            createdAt: new Date().getTime(),
-            isNew: true
-        }]
-        setFiels(newFiles)
+    const deleteFile = (id) => {
+        if (files[id].isNew) {
+            const { [id]: value, ...afterDelete } = files
+            setFiles(afterDelete)
+        } else {
+            fileHelper.deleteFile(files[id].path).then(() => {
+                const { [id]: value, ...afterDelete } = files
+                setFiles(afterDelete)
+                saveFilesToStore(afterDelete)
+                // close the tab if opened
+                tabClose(id)
+            })
+        }
     }
+
+    const updateFileName = (id, title, isNew) => {
+        // newPath should be different based on isNew
+        // if isNew is false, path should be old dirname + new title
+        const newPath = isNew ? join(savedLocation, `${title}.md`)
+            : join(dirname(files[id].path), `${title}.md`)
+        const modifiedFile = { ...files[id], title, isNew: false, path: newPath }
+        const newFiles = { ...files, [id]: modifiedFile }
+        if (isNew) {
+            fileHelper.writeFile(newPath, files[id].body).then(() => {
+                setFiles(newFiles)
+                saveFilesToStore(newFiles)
+            })
+        } else {
+            const oldPath = files[id].path
+            fileHelper.renameFile(oldPath, newPath).then(() => {
+                setFiles(newFiles)
+                saveFilesToStore(newFiles)
+            })
+        }
+
+    }
+
+    const fileSearch = (keyword) => {
+        // filter out the new files based on the keyword
+        const newFiles = filesArr.filter(file => file.title.includes(keyword))
+        setSearchedFiles(newFiles)
+    }
+
+    const createNewFile = () => {
+        const newID = uuidv4()
+        const newFile = {
+            id: newID,
+            title: '',
+            body: '## 请输出 Markdown',
+            createdAt: new Date().getTime(),
+            isNew: true,
+        }
+        setFiles({ ...files, [newID]: newFile })
+    }
+
+    const saveCurrentFile = () => {
+        const { path, body, title } = activeFile
+        fileHelper.writeFile(path, body).then(() => {
+            setUnsavedFileIDs(unsavedFileIDs.filter(id => id !== activeFile.id))
+            if (getAutoSync()) {
+                ipcRenderer.send('upload-file', {key: `${title}.md`, path })
+            }
+        })
+    }
+
+    const importFiles = () => {
+        remote.dialog.showOpenDialog({
+            title: '选择导入的 Markdown 文件',
+            properties: ['openFile', 'multiSelections'],
+            filters: [
+                {name: 'Markdown files', extensions: ['md']}
+            ]
+        }, (paths) => {
+            if (Array.isArray(paths)) {
+                // filter out the path we already have in electron store
+                // ["/Users/liusha/Desktop/name1.md", "/Users/liusha/Desktop/name2.md"]
+                const filteredPaths = paths.filter(path => {
+                    const alreadyAdded = Object.values(files).find(file => {
+                        return file.path === path
+                    })
+                    return !alreadyAdded
+                })
+                // extend the path array to an array contains files info
+                // [{id: '1', path: '', title: ''}, {}]
+                const importFilesArr = filteredPaths.map(path => {
+                    return {
+                        id: uuidv4(),
+                        title: basename(path, extname(path)),
+                        path,
+                    }
+                })
+                // get the new files object in flattenArr
+                const newFiles = { ...files, ...flattenArr(importFilesArr)}
+                // setState and update electron store
+                setFiles(newFiles)
+                saveFilesToStore(newFiles)
+                if (importFilesArr.length > 0) {
+                    remote.dialog.showMessageBox({
+                        type: 'info',
+                        title: `成功导入了${importFilesArr.length}个文件`,
+                        message: `成功导入了${importFilesArr.length}个文件`,
+                    })
+                }
+            }
+        })
+    }
+
+    const activeFileUploaded = () => {
+        const { id } = activeFile
+        const modifiedFile = { ...files[id], isSynced: true, updatedAt: new Date().getTime() }
+        const newFiles = { ...files, [id]: modifiedFile }
+        setFiles(newFiles)
+        saveFilesToStore(newFiles)
+    }
+
+    const activeFileDownloaded = (event, message) => {
+        const currentFile = files[message.id]
+        const { id, path } = currentFile
+        fileHelper.readFile(path).then(value => {
+            let newFile
+            if (message.status === 'download-success') {
+                newFile = { ...files[id], body: value, isLoaded: true, isSynced: true, updatedAt: new Date().getTime() }
+            } else {
+                newFile = { ...files[id], body: value, isLoaded: true}
+            }
+            const newFiles = { ...files, [id]: newFile }
+            setFiles(newFiles)
+            saveFilesToStore(newFiles)
+        })
+    }
+
+    const filesUploaded = () => {
+        const newFiles = objToArr(files).reduce((result, file) => {
+            const currentTime = new Date().getTime()
+            result[file.id] = {
+                ...files[file.id],
+                isSynced: true,
+                updatedAt: currentTime,
+            }
+            return result
+        }, {})
+        setFiles(newFiles)
+        saveFilesToStore(newFiles)
+    }
+
+    useIpcRenderer({
+        'create-new-file': createNewFile,
+        'import-file': importFiles,
+        'save-edit-file': saveCurrentFile,
+        'active-file-uploaded': activeFileUploaded,
+        'file-downloaded': activeFileDownloaded,
+        'files-uploaded': filesUploaded,
+        'loading-status': (message, status) => { setLoading(status) }
+    })
 
     return (
         <div className="App container-fluid px-0">
@@ -112,8 +272,8 @@ function App() {
                     <FileList
                         files={fileListArr}
                         onFileClick={fileClick}
-                        onSaveEdit={saveEdit}
-                        onFileDelete={fileDelete}
+                        onSaveEdit={updateFileName}
+                        onFileDelete={deleteFile}
                     />
                     <div className="row no-gutters button-group">
                         <div className="col">
@@ -121,7 +281,7 @@ function App() {
                                 text="新建"
                                 colorClass="btn-primary"
                                 icon={faPlus}
-                                onBtnClick={createFile}
+                                onBtnClick={createNewFile}
                             />
                         </div>
                         <div className="col">
@@ -129,22 +289,23 @@ function App() {
                                 text="导入"
                                 colorClass="btn-success"
                                 icon={faFileImport}
+                                onBtnClick={importFiles}
                             />
                         </div>
                     </div>
                 </div>
                 <div className="col-8 right-pannel">
-                    {!activeFileId &&
+                    {!activeFile &&
                         <div className="start-page">
                             选择或者创建新的 Markdown 文档
                         </div>
                     }
-                    {activeFileId &&
+                    {activeFile &&
                         <>
                         <TabList
                             files={openedFiles}
-                            activeId={activeFileId}
-                            unsaveIds={unsavedFilesId}
+                            activeId={activeFileID}
+                            unsaveIds={unsavedFileIDs}
                             onTabClick={tabClick}
                             onCloseTab={tabClose}
                         />
--- src/components/FileList.js
+++ src/components/FileList.js
@@ -5,28 +5,62 @@ import { faMarkdown } from '@fortawesome/free-brands-svg-icons'
 
 import PropTypes from 'prop-types'
 
-import keyPressed from '../hooks/useKeyPress'
+import useKeyPress from '../hooks/useKeyPress'
+import useContextMenu from '../hooks/useContextMenu'
+import { getParentNode } from '../utils/helper'
 
 const FileList = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
-    const [editStatus, setEditStatus] = useState(false)
-    const [value, setValue] = useState('')
-    const node = useRef(null)
-
-    const enterPressed = keyPressed(13) // enter键
-    const escPressed = keyPressed(27) // esc键
+    const [ editStatus, setEditStatus ] = useState(false)
+    const [ value, setValue ] = useState('')
+    let node = useRef(null)
+    const enterPressed = useKeyPress(13)
+    const escPressed = useKeyPress(27)
 
     const closeSearch = (editItem) => {
-        setEditStatus(null)
+        setEditStatus(false)
         setValue('')
-        if(editItem.isNew) {
+        // if we are editing a newly created file, we should delete this file when pressing esc
+        if (editItem.isNew) {
             onFileDelete(editItem.id)
         }
     }
 
+    const clickedItem = useContextMenu([
+        {
+            label: '打开',
+            click: () => {
+                const parentElement = getParentNode(clickedItem.current, 'file-item')
+                if (parentElement) {
+                    onFileClick(parentElement.dataset.id)
+                }
+            }
+        },
+        {
+            label: '重命名',
+            click: () => {
+                const parentElement = getParentNode(clickedItem.current, 'file-item')
+                if (parentElement) {
+                    const { id, title } = parentElement.dataset
+                    setEditStatus(id)
+                    setValue(title)
+                }
+            }
+        },
+        {
+            label: '删除',
+            click: () => {
+                const parentElement = getParentNode(clickedItem.current, 'file-item')
+                if (parentElement) {
+                    onFileDelete(parentElement.dataset.id)
+                }
+            }
+        },
+    ], '.file-list', [files])
+
     useEffect(() => {
         const editItem = files.find(file => file.id === editStatus)
-        if(enterPressed && editStatus && value.trim() != '') {
-            onSaveEdit(editItem.id, value)
+        if (enterPressed && editStatus && value.trim() !== '') {
+            onSaveEdit(editItem.id, value, editItem.isNew)
             setEditStatus(false)
             setValue('')
         }
@@ -37,14 +71,14 @@ const FileList = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
 
     useEffect(() => {
         const newFile = files.find(file => file.isNew)
-        if(newFile) {
+        if (newFile) {
             setEditStatus(newFile.id)
             setValue(newFile.title)
         }
-    },[files])
+    }, [files])
 
     useEffect(() => {
-        if(editStatus) {
+        if (editStatus) {
             node.current.focus()
         }
     }, [editStatus])
@@ -54,70 +88,53 @@ const FileList = ({ files, onFileClick, onSaveEdit, onFileDelete }) => {
             {
                 files.map(file => (
                     <li
-                        className="row mx-0 list-group-item bg-light d-flex align-items-center file-item"
+                        className="list-group-item bg-light row d-flex align-items-center file-item mx-0"
                         key={file.id}
+                        data-id={file.id}
+                        data-title={file.title}
                     >
-                        {(file.id !== editStatus && !file.isNew) &&
-                            <>
-                            <span className="col-2">
-                                <FontAwesomeIcon
-                                    icon={faMarkdown}
-                                    size="lg"
-                                />
-                            </span>
-                            <span
-                                className="col-6 c-link"
-                                onClick={()=>{onFileClick(file.id)}}
-                            >{file.title}</span>
-
-                            <button
-                                className="icon-btn col-2"
-                                type="button"
-                                onClick={()=>{setEditStatus(file.id);setValue(file.title)}}
-                            >
-                                <FontAwesomeIcon
-                                    icon={faEdit}
-                                    size="lg"
-                                    title="编辑"
-                                />
-                            </button>
-                            <button
-                                className="icon-btn col-2"
-                                type="button"
-                                onClick={()=>{onFileDelete(file.id)}}
-                            >
-                                <FontAwesomeIcon
-                                    icon={faTrash}
-                                    size="lg"
-                                    title="删除"
-                                />
-                            </button>
-                            </>
-                        }
-                        {((file.id === editStatus) || file.isNew) &&
-                            <>
-                            <input
-                                className="form-control col-10"
-                                value={value}
-                                ref={node}
-                                onChange={(e)=>{setValue(e.target.value)}}
+                        { (file.id !== editStatus && !file.isNew) &&
+                        <>
+                        <span className="col-2">
+                            <FontAwesomeIcon
+                                size="lg"
+                                icon={faMarkdown}
                             />
-                            <button
-                                className="icon-btn col-2"
-                                type="button"
-                                onClick={()=>{closeSearch(file)}}
-                            >
-                                <FontAwesomeIcon
-                                    icon={faTimes}
-                                    title="关闭"
-                                    size="lg"
-                                />
-                            </button>
-                            </>
+                        </span>
+                        <span
+                            className="col-10 c-link"
+                            onClick={() => {onFileClick(file.id)}}
+                        >
+                            {file.title}
+                        </span>
+                        </>
+                        }
+                        { ((file.id === editStatus) || file.isNew) &&
+                        <>
+                        <input
+                            className="form-control col-10"
+                            ref={node}
+                            value={value}
+                            placeholder="请输入文件名称"
+                            onChange={(e) => { setValue(e.target.value) }}
+                        />
+                        <button
+                            type="button"
+                            className="icon-button col-2"
+                            onClick={() => {closeSearch(file)}}
+                        >
+                            <FontAwesomeIcon
+                                title="关闭"
+                                size="lg"
+                                icon={faTimes}
+                            />
+                        </button>
+                        </>
                         }
                     </li>
                 ))
             }
+        </ul>
         </ul>
     )
 }
